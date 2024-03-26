fd5f081f5466a669cd2165ac3fb72b8dcc2e18d5
Nicola <ngrassini@yahoo.it>
2024-03-26 17:33:58 +0000

    • add style for loading state via useNavigation

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 
 import type { LinksFunction } from "@remix-run/node";
@@ -33,6 +34,7 @@ export const action = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
   return (
     <html lang="en">
       <head>
@@ -90,7 +92,10 @@ export default function App() {
           </nav>
         </div>
 
-        <div id="detail">
+        <div
+          className={navigation.state === "loading" ? "loading" : ""}
+          id="detail"
+        >
           {/* Renders the matching child route of a parent route. */}
           <Outlet />
         </div>
